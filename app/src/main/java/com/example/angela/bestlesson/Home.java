763056ad9e0317f25c315fa1469654a2e3e1f740
Nicolò <nicmalate@users.noreply.github.com>
2019-09-17 00:13:56 +0200

    -OrganizzaCalendario new class -activity_organizzazionecalendario new layout -activity_login add scrollView and background adjustment -NoActionBar at top of screen

--- app/src/main/java/com/example/angela/bestlesson/Home.java
+++ app/src/main/java/com/example/angela/bestlesson/Home.java
@@ -1,15 +1,29 @@
 package com.example.angela.bestlesson;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Home extends AppCompatActivity {
 
+    private Button calendarBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        calendarBtn = findViewById(R.id.calendarBtn);
+
+        calendarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, OrganizzaCalendario.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/angela/bestlesson/OrganizzaCalendario.java
@@ -0,0 +1,14 @@
+package com.example.angela.bestlesson;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OrganizzaCalendario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_organizzacalendario);
+    }
+}
