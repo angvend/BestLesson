ebc84202db2a522bd8b8f19b7a543f7796b215e4
Nicolò <nicmalate@users.noreply.github.com>
2019-09-19 17:33:01 +0200

    OrganizzaCalendario view(must update)

--- app/src/main/java/com/example/angela/bestlesson/OrganizzaCalendario.java
+++ app/src/main/java/com/example/angela/bestlesson/OrganizzaCalendario.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class OrganizzaCalendario extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organizzacalendario);
-
     }
 }
