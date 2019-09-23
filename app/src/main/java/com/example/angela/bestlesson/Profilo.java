c9730dad3f3c08c1803203a15740d1bba0c41f8c
Angela <a.venditti1@studenti.unimol.it>
2019-09-23 15:37:52 +0200

    - creato visualizza profilo

---
+++ app/src/main/java/com/example/angela/bestlesson/Profilo.java
@@ -0,0 +1,83 @@
+package com.example.angela.bestlesson;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Profilo extends AppCompatActivity {
+
+    public TextView emailTV, nomeTV, professioneTV;
+    public UserInformation userInformation;
+    private FirebaseAuth mAuth;
+    private DatabaseReference myRef;
+    private FirebaseDatabase mFirebaseDatabase;
+
+    String nome, cognome, professione;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profilo);
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+
+
+        emailTV = (TextView) findViewById(R.id.email);
+        nomeTV = (TextView) findViewById(R.id.nome_profilo);
+        professioneTV = (TextView) findViewById(R.id.professione);
+
+        userInformation = new UserInformation();
+        mAuth = FirebaseAuth.getInstance();
+
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                String userID = mAuth.getCurrentUser().getUid();
+
+                getNomeCognome(dataSnapshot, userID);
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+        String email = mAuth.getCurrentUser().getEmail();
+        emailTV.setText("  " + email);
+    }
+
+    private String getNomeCognome(DataSnapshot dataSnapshot, String userId) {
+
+        for(DataSnapshot ds : dataSnapshot.getChildren()){
+            nome = ds.child(userId).child("nome").getValue().toString();
+            cognome = ds.child(userId).child("cognome").getValue().toString();
+
+            nomeTV.setText("  "+ nome +" "+ cognome);
+
+            userInformation.setTipo(Integer.parseInt(ds.child(userId).child("tipo").getValue().toString()));
+            Integer tipo = userInformation.getTipo();
+
+            if (tipo == 1){
+                professioneTV.setText("Insegnante");
+            }else
+                professioneTV.setText("Studente");
+        }
+        return nome;
+    }
+
+}
